ca03a6d3a795f85aa8dc7434b14ea3ec556065ee
pa4op <artyrik-6@mail.ru>
2023-10-16 17:22:38 +0300

    Index page with search, and books page with output

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import Body, FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import Body, FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -22,6 +22,31 @@ BOOKS = [
 ]
 
 
+#############################################################
+@app.get('/')
+def root():
+	return FileResponse('templates/index.html')
+
+
+
+@app.post('/books', response_class=HTMLResponse)
+async def read_all_books_query(request: Request, prompt: str = Form()):
+	book_list = []
+	for book in BOOKS:
+		if book.get('title').casefold() == prompt.casefold() or book.get('author').casefold() == prompt.casefold() or book.get('category').casefold() == prompt.casefold():
+			book_list.append(book)
+
+	return templates.TemplateResponse("books.html", {"request": request, 'book_list': book_list})
+#############################################################
+
+
+
+
+# #READ
+# @app.get('/books')
+# async def read_all_books():
+# 	return BOOKS
+
 
 #CREATE
 @app.post('/books/create_book')
@@ -29,32 +54,13 @@ async def create_book(new_book=Body()):
 	BOOKS.append(new_book)
 
 
-#READ
-@app.get('/books')
-async def read_all_books():
-	return BOOKS
-
-
-#############################################################
-@app.get('/books/', response_class=HTMLResponse)
-async def read_all_books_query(request: Request, category: str):
-	book_category = []
-	for book in BOOKS:
-		if book.get('category').casefold() == category.casefold():
-			book_category.append(book)
-
-	return templates.TemplateResponse("index.html", {"request": request, 'book_category': book_category})
-#############################################################
-
-
 
 @app.get('/books/{book_title}') #lets fastapi know that at this endpoint, you return the data in the function below it
 async def read_all_books(book_title: str): #async is fairly optional on fastapi
 	for book in BOOKS:
-		
-
 		if book.get('title').casefold() ==  book_title.casefold():
 			return book
+
 
 
 @app.get('/books/{book_author}/') #query params can be used with dynamic params
